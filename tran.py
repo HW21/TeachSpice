51d18555e26b94c6230d280d718f4fbccaf67ce8
Dan Fritchman <daniel.fritchman@gmail.com>
2019-06-24 16:17:12 +0100

    Support arbitrary-length port-lists

--- tran.py
+++ tran.py
@@ -16,6 +16,10 @@ i(k+1) = (v(k+1) - v(k)) * C/dt
 """
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from typing import Dict, AnyStr, SupportsFloat
 
 the_timestep = 1e-12
 
@@ -24,12 +28,24 @@ class Component(object):
     """ Base-class for (potentially non-linear) two-terminal elements. """
 
     linear = False
+    ports = []
 
-    def __init__(self, *, p, n):
-        self.p = p
-        self.n = n
-        self.p.add_conn(self)
-        self.n.add_conn(self)
+    def __init__(self, *, conns: dict):
+        assert list(conns.keys()) == self.ports
+        self.ckt = None
+        self.conns = conns
+        for name, node in conns.items():
+            node.add_conn(self)
+
+    def get_v(self, x) -> Dict[AnyStr, SupportsFloat]:
+        """ Get a dictionary of port-voltages, of the form `port_name`:`voltage`. """
+        v = {}
+        for name, node in self.conns.items():
+            if node.num == 0:
+                v[name] = 0.0
+            else:
+                v[name] = x[node.num - 1]
+        return v
 
     def i(self, v: float) -> float:
         """ Returns the element current as a function of voltage `v`. """
@@ -49,6 +65,9 @@ class Component(object):
     def mna_setup(self, an) -> None:
         pass
 
+    def mna_update(self, an) -> None:
+        pass
+
     def tstep(self, an) -> None:
         pass
 
@@ -56,7 +75,39 @@ class Component(object):
         pass
 
 
+# Port names for common two-terminal components
+TWO_TERM_PORTS = ['p', 'n']
+
+
+def mna_update_nonlinear_twoterm(comp, an):
+    x = an.solver.x
+    p = comp.conns['p']
+    n = comp.conns['n']
+    vp = 0.0 if p.num == 0 else x[p.num - 1]
+    vn = 0.0 if n.num == 0 else x[n.num - 1]
+    v = vp - vn
+    if isinstance(comp, Diode):
+        i = comp.i(v)
+        di_dv = comp.di_dv(v)
+    elif isinstance(comp, Capacitor):
+        # Update the "resistive" part of the cap, e.g. C/dt, and
+        # the "past current" part of the cap, e.g. V[k-1]*C/dt
+        di_dv = comp.dq_dv(v) / the_timestep
+        i = comp.q(v) / the_timestep - v * di_dv
+    if p.num != 0:
+        an.mx.Hg[p.num - 1] += i
+        an.mx.Jg[p.num - 1, p.num - 1] += di_dv
+    if n.num != 0:
+        an.mx.Hg[n.num - 1] += i
+        an.mx.Jg[p.num - 1, p.num - 1] += di_dv
+    if p.num != 0 and n.num != 0:
+        an.mx.Jg[p.num - 1, n.num - 1] -= di_dv
+        an.mx.Jg[n.num - 1, p.num - 1] -= di_dv
+
+
 class Diode(Component):
+    ports = TWO_TERM_PORTS
+
     def __init__(self, *, isat=1e-16, vt=25e-3, **kw):
         super().__init__(**kw)
         self.isat = isat
@@ -75,8 +126,12 @@ class Diode(Component):
         v = max(v, -1.5)
         return v
 
+    def mna_update(self, an) -> None:
+        return mna_update_nonlinear_twoterm(self, an)
+
 
 class Resistor(Component):
+    ports = TWO_TERM_PORTS
     linear = True
 
     def __init__(self, *, r: float, **kw):
@@ -85,16 +140,19 @@ class Resistor(Component):
         self.g = 1 / self.r
 
     def mna_setup(self, an) -> None:
-        if self.p is not an.ckt.node0:
-            an.mx.G[self.p.num - 1, self.p.num - 1] += self.g
-        if self.n is not an.ckt.node0:
-            an.mx.G[self.n.num - 1, self.n.num - 1] += self.g
-        if self.p is not an.ckt.node0 and self.n is not an.ckt.node0:
-            an.mx.G[self.p.num - 1, self.n.num - 1] -= self.g
-            an.mx.G[self.n.num - 1, self.p.num - 1] -= self.g
+        p = self.conns['p']
+        n = self.conns['n']
+        if p.num != 0:
+            an.mx.G[p.num - 1, p.num - 1] += self.g
+        if n.num != 0:
+            an.mx.G[n.num - 1, n.num - 1] += self.g
+        if p.num != 0 and n.num != 0:
+            an.mx.G[p.num - 1, n.num - 1] -= self.g
+            an.mx.G[n.num - 1, p.num - 1] -= self.g
 
 
 class Capacitor(Component):
+    ports = TWO_TERM_PORTS
     linear = False
 
     def __init__(self, *, c: float, **kw):
@@ -108,18 +166,25 @@ class Capacitor(Component):
         return self.c
 
     def tstep(self, an) -> None:
-        vp = 0.0 if self.p is an.ckt.node0 else an.v[self.p.num - 1]
-        vn = 0.0 if self.n is an.ckt.node0 else an.v[self.n.num - 1]
-        v = vp - vn
+        p = self.conns['p'].num
+        n = self.conns['n'].num
+        v = self.get_v(an.v)
+        vd = v['p'] - v['n']
         # Update the "past charge" part of the cap equation
-        rhs = - 1 * self.q(v) / the_timestep
-        if self.p is not an.ckt.node0:
-            an.mx.st[self.p.num - 1] = -1 * rhs
-        if self.n is not an.ckt.node0:
-            an.mx.st[self.n.num - 1] = 1 * rhs
+        rhs = - 1 * self.q(vd) / the_timestep
+        if p != 0:
+            an.mx.st[p - 1] = -1 * rhs
+        if n != 0:
+            an.mx.st[n - 1] = 1 * rhs
+
+    def mna_update(self, an) -> None:
+        return mna_update_nonlinear_twoterm(self, an)
 
 
 class Isrc(Component):
+    """ Constant Current Source """
+
+    ports = TWO_TERM_PORTS
     linear = True
 
     def __init__(self, *, idc: float, **kw):
@@ -128,16 +193,74 @@ class Isrc(Component):
 
     def mna_setup(self, an):
         s = an.mx.s
-        s[self.p.num - 1] = 1 * self.idc
+        p = self.conns['p']
+        s[p.num - 1] = 1 * self.idc
+
+
+class Bjt(Component):
+    ports = ['c', 'b', 'e']
+
+
+class Mos(Component):
+    """ Level-Zero MOS Model """
+    ports = ['g', 'd', 's', 'b']
+    vth = 0.2
+    beta = 1e-6
+    lam = 0  # Sorry "lambda" is a keyword
+
+    def mna_update(self, an) -> None:
+        mx = an.mx
+        v = self.get_v(an.v)
+        # i = self.i(v)
+
+        vds = v['d'] - v['s']
+        vgs = v['g'] - v['s']
+        vov = vgs - self.vth
+        if vov <= 0:  # Cutoff
+            ids = 0
+            gm = 0
+            gds = 0
+        elif vds >= vov:  # Saturation
+            ids = self.beta / 2 * (vov ** 2) * (1 + self.lam * vds)
+            gm = self.beta * vov * (1 + self.lam * vds)
+            gds = self.lam * self.beta / 2 * (vov ** 2)
+        else:  # Triode
+            ids = self.beta * ((vov ** vds) - (vds ** 2) / 2)
+            gm = self.beta * vds
+            gds = self.beta * (vov - vds)
+        # return dict(d=ids, g=0, s=-1 * ids, b=0)
+
+        dn = self.conns['d'].num
+        sn = self.conns['s'].num
+        gn = self.conns['g'].num
+        assert self.conns['b'].num == 0  # No floating bulk, yet
+        if dn != 0:
+            mx.Hg[dn - 1] -= ids
+            mx.Jg[dn - 1, dn - 1] -= gds
+        if sn != 0:
+            mx.Hg[sn - 1] += ids
+            mx.Jg[sn - 1, sn - 1] += gm + gds
+        if dn != 0 and sn != 0:
+            mx.Jg[dn - 1, sn - 1] -= (gm + gds)
+            mx.Jg[sn - 1, dn - 1] -= gds
+        if gn != 0 and sn != 0:
+            mx.Jg[sn - 1, gn - 1] -= gm
+        if gn != 0 and dn != 0:
+            mx.Jg[dn - 1, gn - 1] += gm
+
+    def di_dv(self, v: np.ndarray) -> np.ndarray:
+        """ Returns a 4x4 matrix of current-derivatives """
+        # FIXME!
+        return np.zeros((4, 4))
 
 
 class Node(object):
-    """ The one and only Node in our circuit.
-    Represents its connected elements, and knows how to perform KCL and its derivative. """
+    """ Circuit Node """
 
     def __init__(self):
         self.conns = []
         self.num = None
+        self.ckt = None
 
     def add_conn(self, conn: Component):
         assert isinstance(conn, Component)
@@ -153,9 +276,12 @@ class Circuit(object):
     """ Circuit Under Test for Simulation. """
 
     def __init__(self):
-        self.node0 = Node()
-        self.nodes = [self.node0]
         self.comps = []
+        self.nodes = []
+        # Create node zero, and initialize our node-list
+        self.node0 = Node()
+        self.add_node(self.node0)
+        # Run any custom definition
         self.define()
 
     def define(self) -> None:
@@ -169,25 +295,28 @@ class Circuit(object):
     def add_node(self, node: Node) -> int:
         assert isinstance(node, Node)
         node.num = len(self.nodes)
+        node.ckt = self
         self.nodes.append(node)
         return len(self.nodes) - 1
 
-    def create_comp(self, *, cls: type, p: Node, n: Node, **kw) -> Component:
+    def create_comp(self, *, cls: type, **kw) -> Component:
         """ Create and add Component of class `cls`, and return it. """
-        comp = cls(p=p, n=n, **kw)
+        comp = cls(**kw)
+        comp.ckt = self
         return self.add_comp(comp=comp)
 
     def add_comp(self, comp: Component) -> Component:
         """ Add Component `comp`, and return it. """
         assert isinstance(comp, Component)
-        assert comp.p in self.nodes
-        assert comp.n in self.nodes
+        for name, node in comp.conns.items():
+            assert node in self.nodes
         self.comps.append(comp)
         return comp
 
 
 class MnaSystem(object):
-    """ Represents the non-linear matrix-equation
+    """
+    Represents the non-linear matrix-equation
     G*x + H*g(x) = s
 
     And the attendant break-downs which help us solve it.
@@ -215,28 +344,7 @@ class MnaSystem(object):
         self.Jg = np.zeros((self.num_nodes, self.num_nodes))
         self.Hg = np.zeros(self.num_nodes)
         for comp in self.ckt.comps:
-            if comp.linear:
-                continue
-            vp = 0.0 if comp.p is self.ckt.node0 else x[comp.p.num - 1]
-            vn = 0.0 if comp.n is self.ckt.node0 else x[comp.n.num - 1]
-            v = vp - vn
-            if isinstance(comp, Diode):
-                i = comp.i(v)
-                di_dv = comp.di_dv(v)
-            elif isinstance(comp, Capacitor):
-                # Update the "resistive" part of the cap, e.g. C/dt, and
-                # the "past current" part of the cap, e.g. V[k-1]*C/dt
-                di_dv = comp.dq_dv(v) / the_timestep
-                i = comp.q(v) / the_timestep - v * di_dv
-            if comp.p is not self.ckt.node0:
-                self.Hg[comp.p.num - 1] += i
-                self.Jg[comp.p.num - 1, comp.p.num - 1] += di_dv
-            if comp.n is not self.ckt.node0:
-                self.Hg[comp.n.num - 1] += i
-                self.Jg[comp.p.num - 1, comp.p.num - 1] += di_dv
-            if comp.p is not self.ckt.node0 and comp.n is not self.ckt.node0:
-                self.Jg[comp.p.num - 1, comp.n.num - 1] -= di_dv
-                self.Jg[comp.n.num - 1, self.p.num - 1] -= di_dv
+            comp.mna_update(self.an)
 
     def res(self, x: np.ndarray) -> np.ndarray:
         """ Return the residual error, given solution `x`. """
@@ -267,9 +375,7 @@ class Solver:
         self.update()
         # Solve Jf(x) * dx + f(x) = 0
         dx = self.mx.solve(self.x)
-        # print(f'Updating by {dx}')
         self.x += dx
-        # vl = self.node.limit(vn)
         self.history.append(self.x)
 
     def converged(self) -> bool:
@@ -328,6 +434,7 @@ class Tran(Analysis):
         super().__init__(ckt)
         self.v = np.zeros(self.mx.num_nodes)
         self.history = [self.v]
+        self.solver = None
 
     def update(self):
         """ Update time-dependent (dynamic) circuit element terms. """
@@ -338,8 +445,8 @@ class Tran(Analysis):
 
     def iterate(self):
         """ Run a single Newton solver """
-        s = Solver(mx=self.mx)
-        v = s.solve()
+        self.solver = Solver(mx=self.mx)
+        v = self.solver.solve()
         # print(f'New Solution {v}')
         self.v = v
         self.history.append(self.v)
@@ -352,18 +459,44 @@ class Tran(Analysis):
             self.iterate()
 
 
+def rc_filter(c=1e-12, num_nodes=5):
+    class RcFilter(Circuit):
+        """ Current-Driven RC Filter, 
+        with Parameterizable R, C, and # intermediate nodes """
+
+        def define(self):
+            self.create_nodes(num_nodes)
+
+            for k in range(num_nodes):
+                self.create_comp(cls=Resistor,
+                                 r=(num_nodes - 1) * 1e3,
+                                 conns=dict(
+                                     p=self.nodes[k + 1],
+                                     n=self.nodes[k],
+                                 ), )
+
+            self.create_comp(cls=Isrc,
+                             idc=1e-3 / (num_nodes - 1),
+                             conns=dict(
+                                 p=self.nodes[num_nodes],
+                                 n=self.node0,
+                             ), )
+
+            self.create_comp(cls=Capacitor,
+                             c=c,
+                             conns=dict(
+                                 p=self.nodes[1],
+                                 n=self.node0,
+                             ), )
+            # self.create_comp(cls=Diode, p=self.nodes[1], n=self.node0)
+
+    return RcFilter()
+
+
 def sim(c=1e-12):
     """ Create an instance of the circuit under test, and simulate it. """
-    ckt = Circuit()
     num_nodes = 5
-    ckt.create_nodes(num_nodes)
-
-    for k in range(num_nodes):
-        ckt.create_comp(cls=Resistor, p=ckt.nodes[k + 1], n=ckt.nodes[k], r=(num_nodes - 1) * 1e3)
-
-    ckt.create_comp(cls=Isrc, p=ckt.nodes[num_nodes], n=ckt.node0, idc=1e-3 / (num_nodes - 1))
-    ckt.create_comp(cls=Capacitor, p=ckt.nodes[1], n=ckt.node0, c=c)
-    # ckt.create_comp(cls=Diode, p=ckt.nodes[1], n=ckt.node0)
+    ckt = rc_filter(c=c, num_nodes=num_nodes)
 
     s = Tran(ckt=ckt)
     s.solve()
@@ -372,15 +505,19 @@ def sim(c=1e-12):
 
 
 def main():
-    import pandas as pd
-    import matplotlib.pyplot as plt
+    """ Run some sims!
+    Sweep and plot a few RC parameters. """
+
+    def add_tran_plots(sim):
+        df = pd.DataFrame.from_records(sim.history)
+        # print(df)
+        plt.plot(df[0])
+        plt.plot(df[1])
 
     for c in 1e-14, 1e-13, 1e-12:
         s = sim(c=c)
-        df = pd.DataFrame.from_records(s.history)
-        print(df)
-        plt.plot(df[0])
-        plt.plot(df[1])
+        add_tran_plots(s)
+
     plt.show()
 
 
